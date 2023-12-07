26f9e580b1ef610ac28df217e327dab284fd65bf
BrunaSaNu <bruna.santana.0511@gmail.com>
2023-12-07 14:07:20 -0300

    subindo projeto

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //System.out.printf("Hello and welcome!");
+
+        //Atividade 1
+        //float salario = 10000.00f;
+        //float abono = 1000.00f;
+        //System.out.println("Novo salário = " + (salario + abono));
+
+
+        //Atividade 2
+        //float nota1 = 10.0f;
+        //float nota2 = 8.0f;
+        //float nota3 = 7.0f;
+        //float nota4 = 7.5f;
+        //System.out.println("Sua média final é: " + ((nota1 + nota2 + nota3 + nota4) / 4));
+
+
+        //Atividade 3
+        //float salarioBruto = 2000.00f;
+        //float adicionalNoturno = 500.00f;
+        //float horasExtras = 100.00f;
+        //float descontos = 200.00f;
+        //System.out.println("Salário líquido = " + (salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos));
+
+        //Atividade 4
+        float numero1 = 5.0f;
+        float numero2 = 6.0f;
+        float numero3 = 7.0f;
+        float numero4 = 8.0f;
+        System.out.println("A diferença de valores entre os produtos é: " + (numero1 * numero2 - numero3 * numero4));
+
+        float produto1 = 5.0f;
+        float produto2 = 6.0f;
+        float produto3 =  -7.0f;
+        float produto4 = 8.0f;
+        System.out.println("A diferença de valores entre os produtos é: " + (produto1 * produto2 - produto3 * produto4));
+
+    }
+}
